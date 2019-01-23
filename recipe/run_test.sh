c98e3cd18cbc90c0fba790a7fa70f61f9ed98ee8
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-23 13:18:53 -0500

    added more stack test commands

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,6 +4,9 @@ set -eu -o pipefail -x
 
 stack new helloworld new-template
 cd helloworld
-stack build
 
-stack exec helloworld-exe | grep someFunc
+STACK_OPTS="-v --local-bin-path ${PREFIX}/bin --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --with-gcc $CC"
+stack ${STACK_OPTS} build --ghc-options "-optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib"
+
+stack ${STACK_OPTS} exec helloworld-exe | grep someFunc
+stack ${STACK_OPTS} test
