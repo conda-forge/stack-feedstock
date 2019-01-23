ad52f29a3d78f226e2ebf4e235bc591446ec2cea
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-23 13:37:02 -0500

    drop with-gcc; add other reqs

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail -x
 stack new helloworld new-template
 cd helloworld
 
-STACK_OPTS="-v --local-bin-path ${PREFIX}/bin --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --with-gcc $CC"
+STACK_OPTS="-v --local-bin-path ${PREFIX}/bin --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib"
 stack ${STACK_OPTS} build --ghc-options "-optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib"
 
 stack ${STACK_OPTS} exec helloworld-exe | grep someFunc
