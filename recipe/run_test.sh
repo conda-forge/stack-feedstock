5e4ad7a80a76c0baf6dc392b75e7f115857437a6
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-29 14:43:09 -0500

    set resolver to lts-8.24

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -5,9 +5,7 @@ set -eu -o pipefail -x
 stack --help
 stack new helloworld new-template
 cd helloworld
-STACK_OPTS=" --resolver 1.9.3 "
+STACK_OPTS=" --resolver lts-8.24 "
 stack $STACK_OPTS setup
 stack $STACK_OPTS build
 stack $STACK_OPTS exec helloworld-exe
-
-
